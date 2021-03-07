21200911ab8d0f935286bc106ab02965ede867c3
Guillermo Felipetti <guill388@hotmail.com>
2021-03-07 18:13:54 -0300

    1

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,21 @@ import Data.List
 
 laVerdad = True
 
+funcion x y lista = (filter (> x) . map (\ f -> f y)) lista
+
+-- FINAL 14/12/19
+
+type ListaDeFunciones = [Int -> Int]
+type Numero = Int
+type ListaDeNumeros = [Int]
+
+aplicarFunciones :: Numero -> ListaDeFunciones -> ListaDeNumeros
+aplicarFunciones numero listaDeFunciones = map (\f -> f numero) listaDeFunciones 
+
+filtrarLosMayoresA :: ListaDeNumeros -> Numero -> ListaDeNumeros
+filtrarLosMayoresA listaDeNumeros numeroAevaluar = filter (> numeroAevaluar) listaDeNumeros
+
+aplicarYfiltrar :: Numero -> Numero -> ListaDeFunciones -> ListaDeNumeros
+aplicarYfiltrar numero numeroAevaluar listaDeFunciones = filtrarLosMayoresA (aplicarFunciones numero listaDeFunciones) numeroAevaluar
+
+-- FINAL 23/02/2019
