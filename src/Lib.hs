a071f9279b61d64ac82a7468ce671bd7f9989eaf
Guillermo Felipetti <guill388@hotmail.com>
2021-03-09 13:57:51 -0300

    cfedwf

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,8 +4,6 @@ import Data.List
 
 laVerdad = True
 
-funcion x y lista = (filter (> x) . map (\ f -> f y)) lista
-
 -- FINAL 14/12/19
 
 type ListaDeFunciones = [Int -> Int]
@@ -26,3 +24,9 @@ aplicarFunciones numero listaDeFunciones = map (\f -> f numero) listaDeFunciones
 f1 h = any (>15). map h   
 
 fi h = (>0). length. filter (>15) . map h 
+
+--1. Ambas funciones llegan al mismo resultado pero de diferente forma, no hacen lo mismo las funciones que la componen. Especie de equifinalidad.
+--2. La primera funcion llega a un resultado si tiene una lista infinita ya que gracias a Lazy Evaluation si detecta aunque sea un numero mayor a 15, deja de evaluar el resto
+-- Si le pones la lista [-99,-100..] y de la funcion h es (+1), obviamente nunca va a llegar a un resultado, porque va a seguir evaluando infinitamente. Depende mucho de h tmb.
+-- La segunda función no llega nunca a un resultado ya que ese filter, necesita si o si toda la lista. 
+--3. Las 2 son de orden superior, ya que las 2 son funciones que reciben o entregan funciones.
